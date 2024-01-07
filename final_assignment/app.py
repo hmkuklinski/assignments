4a360b5360bfcd48dde633c0265e49b1a0cb9a47
hmkuklinski <hmkuklinski@gmail.com>
2024-01-07 02:15:23 -0500

    added html/css formatting files, added flashcard components, removed the chart on main page to add to separate page

--- final_assignment/app.py
+++ final_assignment/app.py
@@ -9,7 +9,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html",flashcards= flashcards) #passes in the flashcard dictionary
+    json_file = 'words.json'
+    try:
+        with open(json_file, 'r') as file:
+            data= json.load(file)
+    except json.decoder.JSONDecodeError:
+        data= {}
+    return render_template("index.html",flashcards= data) #passes in the flashcard dictionary
 
 @app.route("/add_flashcard", methods=["POST", "GET"])
 def add():
@@ -18,15 +24,29 @@ def add():
     translation = translator.translate(eng_word) #translate to english
     flashcards[eng_word]= translation #add to dictionary
     
+    json_file = 'words.json'
+    with open(json_file, 'w') as file:
+        json.dump(flashcards, file)
+    
     return redirect("/")
+
 
 @app.route("/remove", methods=["POST"])
 def remove():
     word= request.form.get("word")
     print(f"Word received by request: {word}")
     if word in flashcards:
+        print(f"Removing flashcard: {flashcards[word]}")
         del flashcards[word]
-    return redirect("/")
+        print(f"Flashcards after removal: {flashcards}")
+        #need to edit the json file with the removal
+        json_file = 'words.json'
+        with open(json_file, 'w') as file:
+            json.dump(flashcards, file)
+    else:
+        print(f"Flashcard with word '{word}' is not found.")
+            
+    return redirect(request.referrer)
 
 if __name__== '__main__':
     app.run(debug=True)
